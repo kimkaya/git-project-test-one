a48304d700aa63c8f2c6d17f498a7d2f46510d1d
kimkaya <sanikawakayari@naver.com>
2022-12-12 17:40:46 +0900

    mainpage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Board from "./pages/Board";
+import BoardDetail from "./components/Board/BoardDetail";
+import MyPage from "./pages/MyPage";
+import Auth from "./hoc/auth";
+
+const Container = styled.div`
+  margin: 10px auto;
+  width: 370px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Auth(Login, false)}></Route>
+          <Route path="/register" component={Auth(Register, false)} />
+          <Route path="/board" component={Auth(Board, true)} />
+          <Route path="/board/:boardId" component={Auth(BoardDetail, true)} />
+          <Route path="/mypage" component={Auth(MyPage, true)} />
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
